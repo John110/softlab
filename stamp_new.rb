f18bce540e0b18108c1ea80a201bb0035ea66fc7
John <monaeru0612@hotmail.co.jp>
2014-03-29 00:11:04 +0900

    stamp
    先輩のプログラムを参考にmap dataの読み込みのメソッドをnumber_of_islands,islandsに変更
    その他メソッド名を調整
    mainでcallされないメソッドをprivate化
    .nil?等細かい表記の変更

--- stamp_new.rb
+++ stamp_new.rb
@@ -1,144 +1,108 @@
-class Scan
+class Searcher
   def initialize(max,map)
     @max = max
     @map = map
   end
 
-  def scanning_start
+  def stamp_rally
+    route = []
+    search_first_island
+    loop do
+      route << @start
+      delete_with_island(@start)
+      neighbor = @map[@start]
+      next_island = search_route(neighbor)
+      if next_island.nil?
+        puts "nothing neighbor island"
+        route << neighbor[0]
+        break
+      end
+      neighbor = next_island
+      @start = neighbor
+    end
+    return route
+  end
+
+private
+  def search_first_island
     @start = nil
     min = @max
     @max.times do |i|
-      
       if @map[i].size<min && @map[i].size != 0 then
         min = @map[i].size
         @start = i
       end
-
     end
-
-    return @start
-
   end
 
-  def scanning_route(neighbor)
+  def search_route(neighbor)
     @next_island_num = nil
     @count_max = 0
     @far_island = nil
     @neighbor = neighbor
-
     check_next_island
-
-    if @next_island_num == nil then
+    if @next_island_num.nil?
       return nil
     else
       return @neighbor[@next_island_num]
     end
-
   end
 
   def check_next_island
     @neighbor.each_with_index do |next_island, i|
       count = 1
       @far_island = @map[next_island][i]
-
       @max.times do |j|
-
-        if @far_island == nil
+        if @far_island.nil?
           break
         end
-
         count += 1
         @far_island = @map[@far_island][j]
-
         if count > @count_max && @far_island != nil then
           @count_max = count
           @next_island_num = i
         end
-
       end
+    end
+  end
   
-    end
-
-  end
-
-  def stamp_rally
-    route = []
-    loop{
-      route << @start
-      island_delete(@start)
-      neighbor = @map[@start]
-      next_island = scanning_route(neighbor)
-
-      if next_island == nil then
-        puts "nothing neighbor island"
-        route << neighbor[0]
-        break
-      end
-
-      neighbor = next_island
-      @start = neighbor
-    }
-    return route
-  end
-
-  def island_delete(start)
-
+  def delete_with_island(island)
     @max.times do |i|
-      @map[i].delete(start)
+      @map[i].delete(island)
     end
-
   end
-
 end
 
-class FileAccess
-  def initialize(filename1, filename2)
-    @map_data = filename1
-    @stampsheet = filename2
-    @map = []
+class FileManager
+  def read(file_name)
+    @input_file = open(file_name,"r")
+    map_data = [number_of_islands,islands]
+    @input_file.close
+    return map_data
   end
 
-  def map_open
-    return open(@map_data,"r")
+  def write_with_route(route)
+    f = open("stampsheet.txt","w")
+    f.puts(route)
+    f.close
   end
 
-  def map_load
-  	f = map_open
-    max = f.gets.chomp.to_i
-    map_all = f.readlines
+private
+  def number_of_islands
+    return @input_file.gets.chomp.to_i
+  end
 
-    max.times do |i|
-      @map[i] = map_all[i].split(/\s/)
-      @map[i].map!{|array| array.to_i(10)}
+  def islands
+    map = []
+    @input_file.readlines.each do |map_data|
+      map << map_data.split(" ").map{|array| array.to_i}
     end
-
-    return max, @map
+    return map
   end
-
-  def map_close
-    return map_open.close
-  end
-
-  def stampsheet_open
-    return open(@stampsheet,"w")
-  end
-
-  def stampsheet_write(route)
-    return stampsheet_open.puts(route)
-  end
-
-  def stampsheet_close
-    return stampsheet_open.close
-  end
-
 end
 
-map = []
-file_access = FileAccess.new("map.txt", "stampsheet.txt")
-max,map = file_access.map_load
-file_access.map_close
-scan = Scan.new(max, map)
-scan.scanning_start
-route = scan.stamp_rally
-file_access.stampsheet_write(route)
-file_access.stampsheet_close
+file_manager = FileManager.new
+max,map = file_manager.read("map.txt")
+searcher = Searcher.new(max, map)
+route = searcher.stamp_rally
+file_manager.write_with_route(route)
