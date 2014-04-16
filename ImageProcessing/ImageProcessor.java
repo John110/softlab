ac833f37ff629a4cf9fc31e416e6518d78452441
John <monaeru0612@hotmail.co.jp>
2014-04-16 17:44:37 +0900

    画像処理プログラム
    フィルタリング処理部をFilterクラスに移動

--- ImageProcessing/ImageProcessor.java
+++ ImageProcessing/ImageProcessor.java
@@ -1,4 +1,5 @@
 import static img.ImageManager.*;
+import static img.Filter.*;
 
 import java.awt.image.BufferedImage;
 import java.io.*;
@@ -6,11 +7,11 @@ import java.io.*;
 
 public class ImageProcessor{
 
-    public static void main(String[] args) throws IOException{
-        set_image("test.jpg");
-        int mode = Integer.parseInt(args[0]);
-        if(mode == 1){
-        	negapoji();
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		set_image("test.jpg");
+		int mode = Integer.parseInt(args[0]);
+		if(mode == 1){
+			negapoji();
+		}
+	}
 }
--- ImageProcessing/img/ImageManager.java
+++ ImageProcessing/img/ImageManager.java
@@ -8,48 +8,33 @@ public class ImageManager{
 	static BufferedImage writer;
 	static int width;
 	static int height;
-	
+
 	public static void set_image(String file_name) throws IOException{
 		reader = ImageIO.read(new File(file_name));
 		width = reader.getWidth();
 		height = reader.getHeight();
 		writer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	}
-	
-	public static void negapoji() throws IOException{
-		for(int y=0;y<height;y++){
-			for(int x=0;x<width;x++){
-				int c = reader.getRGB(x, y);
-				int r = 255-r(c);
-				int g = 255-g(c);
-				int b = 255-b(c);
-				int rgb = rgb(r,g,b);
-				writer.setRGB(x,y,rgb);
-			}
-		}
-		File output_image = new File("negapoji.jpg");
-		ImageIO.write(writer, "jpg", output_image);
+
+	public static int a(int c){
+		return c>>>24;
 	}
-	
-    public static int a(int c){
-        return c>>>24;
-    }
-    public static int r(int c){
-        return c>>16&0xff;
-    }
-    public static int g(int c){
-        return c>>8&0xff;
-    }
-    public static int b(int c){
-        return c&0xff;
-    }
-    public static int rgb
-    (int r,int g,int b){
-        return 0xff000000 | r <<16 | g <<8 | b;
-    }
-    public static int argb
-    (int a,int r,int g,int b){
-        return a<<24 | r <<16 | g <<8 | b;
-    }
+	public static int r(int c){
+		return c>>16&0xff;
+	}
+	public static int g(int c){
+		return c>>8&0xff;
+	}
+	public static int b(int c){
+		return c&0xff;
+	}
+	public static int rgb
+	(int r,int g,int b){
+		return 0xff000000 | r <<16 | g <<8 | b;
+	}
+	public static int argb
+	(int a,int r,int g,int b){
+		return a<<24 | r <<16 | g <<8 | b;
+	}
 
 }
