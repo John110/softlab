b99bdae8579b82220705ac0533052421f16d7b0d
John <monaeru0612@hotmail.co.jp>
2014-08-05 20:53:30 +0900

    OpenCV Demo

--- lab_demo/filter.py
+++ lab_demo/filter.py
@@ -25,43 +25,43 @@ class Filter:
     cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_WIDTH, self.WIDTH)
     cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_HEIGHT, self.HEIGHT)
     while True:
-      image_of_source = cv.QueryFrame(self.capture)
-      image_of_dest = cv.CloneImage(image_of_source)
+      source_image = cv.QueryFrame(self.capture)
+      dest_image = cv.CloneImage(source_image)
       flag = cv.WaitKey(1)
       if flag != -1 :
         mode = flag
       if mode == self.KEY_1:
-        cv.Not(image_of_source, image_of_dest)
+        cv.Not(source_image, dest_image)
       elif mode == self.KEY_2:
-        cv.Smooth(image_of_source, image_of_dest, cv.CV_BLUR, 10, 10)
+        cv.Smooth(source_image, dest_image, cv.CV_BLUR, 10, 10)
       elif mode == self.KEY_3:
         model = 10
         for i in xrange(0, self.HEIGHT - 1):
           for j in xrange(0, self.WIDTH - 1):
-            bgr = cv.Get2D(image_of_source, i, j)
-            next_pixel_bgr = cv.Get2D(image_of_source, i + 1, j + 1)
+            bgr = cv.Get2D(source_image, i, j)
+            next_pixel_bgr = cv.Get2D(source_image, i + 1, j + 1)
             if abs(bgr[0] - next_pixel_bgr[0]) > model or abs(bgr[1] - next_pixel_bgr[1]) > model or abs(bgr[2] - next_pixel_bgr[2]) > model:
-              cv.Set2D(image_of_dest, i, j, (0, 0, 0))
+              cv.Set2D(dest_image, i, j, (0, 0, 0))
             else:
-              cv.Set2D(image_of_dest, i, j, (255, 255, 255))
+              cv.Set2D(dest_image, i, j, (255, 255, 255))
       elif mode == self.KEY_4:
         for i in xrange(0, self.HEIGHT):
           for j in xrange(0, self.WIDTH):
-            bgr = cv.Get2D(image_of_source, i, j)
+            bgr = cv.Get2D(source_image, i, j)
             if bgr[2] > bgr[1] and bgr[2] > bgr[0]:
-              cv.Set2D(image_of_dest, i, j, (0, 0, 255))
+              cv.Set2D(dest_image, i, j, (0, 0, 255))
             elif bgr[1] > bgr[0]:
-              cv.Set2D(image_of_dest, i, j, (0, 255, 0))
+              cv.Set2D(dest_image, i, j, (0, 255, 0))
             else:
-              cv.Set2D(image_of_dest, i, j, (255, 0, 0))
+              cv.Set2D(dest_image, i, j, (255, 0, 0))
       elif mode == self.KEY_5:
-        face = cv.HaarDetectObjects(image_of_dest, self.detector, cv.CreateMemStorage(0), HAAR_SCALE, MIN_NEIGHBORS, HAAR_FLAGS, MIN_SIZE)
+        face = cv.HaarDetectObjects(dest_image, self.detector, cv.CreateMemStorage(0), HAAR_SCALE, MIN_NEIGHBORS, HAAR_FLAGS, MIN_SIZE)
         for ((x, y, w, h), n) in face:
-          cv.Rectangle(image_of_dest, (x, y), (x + w, y + h), cv.RGB(255, 0, 0), 1, 0)
+          cv.Rectangle(dest_image, (x, y), (x + w, y + h), cv.RGB(255, 0, 0), 1, 0)
       elif mode == self.KEY_Q:
         break
-      cv.ShowImage("SourceImage", image_of_source)
-      cv.ShowImage("DestImage", image_of_dest) 
+      cv.ShowImage("SourceImage", source_image)
+      cv.ShowImage("DestImage", dest_image) 
     cv.DestroyAllWindows()
 
 
--- lab_demo/filter.rb
+++ lab_demo/filter.rb
@@ -17,27 +17,27 @@ class Filter
 	def process
 		mode = 0
 		loop do
-			@image_of_capture = @capture.query.resize OpenCV::CvSize.new(@WIDTH, @HEIGHT)
-			@image_of_dest = @image_of_capture
+			@capture_image = @capture.query.resize OpenCV::CvSize.new(@WIDTH, @HEIGHT)
+			@dest_image = @capture_image
 			input_key = OpenCV::GUI::wait_key(1)
 			if input_key != nil 
 				mode = input_key
 			end
 			case mode
 			when @KEY_1
-				@image_of_dest = @image_of_dest.not
+				@dest_image = @dest_image.not
 			when @KEY_2
-				@image_of_dest = @image_of_dest.smooth(:blur, 10, 10)
+				@dest_image = @dest_image.smooth(:blur, 10, 10)
 			when @KEY_3
-				@detector.detect_objects(@image_of_capture).each do |rect|
+				@detector.detect_objects(@capture_image).each do |rect|
 					puts "detect!! : #{rect.top_left}, #{rect.top_right}, #{rect.bottom_left}, #{rect.bottom_right}"
-					@image_of_dest = @image_of_dest.rectangle(rect.top_left, rect.bottom_right, :color => OpenCV::CvColor::Red)
+					@dest_image = @dest_image.rectangle(rect.top_left, rect.bottom_right, :color => OpenCV::CvColor::Red)
 				end
 			when @KEY_Q
 				break
 			end
-			@source_window.show(@image_of_capture)
-			@dest_window.show(@image_of_dest)
+			@source_window.show(@capture_image)
+			@dest_window.show(@dest_image)
 		end
 		OpenCV::GUI::Window.destroy_all
 	end
